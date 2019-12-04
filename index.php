2a49889bd67b952e4a60016670cafe882e1b52b3
AuburnFord <auburnford@gmail.com>
2019-12-03 21:56:43 -0600

    Final version

--- index.php
+++ index.php
@@ -9,45 +9,37 @@ $result = NULL;
 $error = NULL;
 
 if(isset($_POST["query"])) {
-	$query = $_POST["query"];			
-	if(!$result = $conn->query($query)) {
+	$query = stripslashes(trim($_POST["query"]));
+	if(strcasecmp('drop', substr($query,0,4))==0) {
+		$error = "'DROP' Statements not allowed";	
+	} elseif(!$result = $conn->query($query)) {
 		$error = $conn->error;
 	}
 }
+
+$first_tab = "db_bookButton";
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Online Bookstore</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
 <style>
-body {font-family: Arial;}
-
-.column {
-	float: left;
-	width: 45%;
-}
-
-.space {
-	float: left;
-	width: 10%;
-}
-
-.row:after {
-	content: "";
-	display: table;
-	clear: block;
-}
-
-/* Style the tab */
+ /* Style the tab */
 .tab {
   overflow: hidden;
   border: 1px solid #ccc;
   background-color: #f1f1f1;
 }
 
-/* Style the buttons inside the tab */
+/* Style the buttons that are used to open the tab content */
 .tab button {
   background-color: inherit;
   float: left;
@@ -56,7 +48,6 @@ body {font-family: Arial;}
   cursor: pointer;
   padding: 14px 16px;
   transition: 0.3s;
-  font-size: 17px;
 }
 
 /* Change background color of buttons on hover */
@@ -75,119 +66,57 @@ body {font-family: Arial;}
   padding: 6px 12px;
   border: 1px solid #ccc;
   border-top: none;
-}
-</style>
-</head>
-<body>
+} </style>
 
+</head>
+
+<body>
+<div class="container-fluid">
+	<h1>Bookstore Database</h1>
+</div>
 
 <div class="row">
-	<div class="column">
+	<!--Tab Content-->
+	<div class="col">
 		<h2>Database</h2>
-		
-		<div class="tab">
-		  <button class="tablinks" onclick="openTab(event, 'Book','BookButton')" id="BookButton">Book</button>
-		  <button class="tablinks" onclick="openTab(event, 'Customer','CustomerButton')"id="CustomerButton">Customer</button>
-		  <button class="tablinks" onclick="openTab(event, 'Employee','EmployeeButton')"id="EmployeeButton">Employee</button>
-		  <button class="tablinks" onclick="openTab(event, 'Order Details','OrderDetailsButton')"id="OrderDetailsButton">Order Details</button>
-		  <button class="tablinks" onclick="openTab(event, 'Orders','OrdersButton')"id="OrdersButton">Orders</button>
-		  <button class="tablinks" onclick="openTab(event, 'Shipper','ShipperButton')"id="ShipperButton">Shipper</button>
-		  <button class="tablinks" onclick="openTab(event, 'Subject','SubjectButton')"id="SubjectButton">Subject</button>
-		  <button class="tablinks" onclick="openTab(event, 'Supplier','SupplierButton')"id="SupplierButton">Supplier</button>
-		</div>
-		
-		<div id="Book" class="tabcontent">
-			<table style="width: 90%">
-				<?php
-					BuildTable($conn, "book");
-				?>
-			</table>
-		</div>
-
-		<div id="Customer" class="tabcontent">
-			<table style="width: 90%">
-				<?php
-					BuildTable($conn, "customer");
-				?>
-			</table>
-		</div>
-
-		<div id="Employee" class="tabcontent">
-			<table style="width: 90%">
-				<?php
-					BuildTable($conn, "employee");
-				?>
-			</table>
-		</div>
-
-		<div id="Order Details" class="tabcontent">
-			<table style="width: 90%">
-				<?php
-					BuildTable($conn, "order_details");
-				?>
-			</table>
-		</div>
-
-		<div id="Orders" class="tabcontent">
-			<table style="width: 90%">
-				<?php
-					BuildTable($conn, "orders");
-				?>
-			</table>
-		</div>
-
-		<div id="Shipper" class="tabcontent">
-			<table style="width: 90%">
-				<?php
-					BuildTable($conn, "shipper");
-				?>
-			</table>
-		</div>
-
-		<div id="Subject" class="tabcontent">
-			<table style="width: 90%">
-				<?php
-					BuildTable($conn, "subject");
-				?>
-			</table>
-		</div>
-
-		<div id="Supplier" class="tabcontent">
-			<table style="width: 90%">
-				<?php
-					BuildTable($conn, "supplier");
-				?>
-			</table>
-		</div>
+		<?php buildTabs($conn); ?>
 	</div>
-	<div class="space">&nbsp</div>
-	<div class="column">
-		<form action="index.php" method="post">
-			<h2>Query</h2>
-			<textarea name="query" rows="15" style="width:80%;" placeholder="Query..."></textarea>
+
+	<!--Query Field-->
+	<div class="col">
+		<h2>Query</h2>
+		<form action="index.php" method="post"><?php
+			if(isset($query)) {
+				echo "<textarea name=\"query\" rows=\"15\" style=\"width:80%;\" placeholder=\"Query...\">" . $query;
+			} else {
+				echo "<textarea name=\"query\" rows=\"15\" style=\"width:80%;\" placeholder=\"Query...\">";
+			}
+			?></textarea>
 			<input type="hidden" name= "tabState" id="tabState" value=<?php
 				if (isset($_POST["tabState"])) {
 					echo $_POST["tabState"];
 				} else {
-					echo "BookButton";
+					echo $first_tab;
 				}
 			?>>
 			<br>
-			<input type="submit">
+			<input type="submit" class="btn btn-success">
 		</form>
-		<h1>	
 		<?php
 			if(!is_null($error)) {
-				echo $error;
-			}
+				echo "<div class=\"alert alert-danger\"><p class=\"text-danger\">" . $error . "</p></div>";
+			} elseif($result != null) {
+				echo "<div class=\"alert alert-success\"><p class=\"text-success\">Successful query!</p></div>";
+				if ($result instanceof mysqli_result) {
+					buildTableFromResult($result);
+				}
+			} 
 		?>
-		</h1>
 	</div>
 </div>
 
-
 <script>
-function openTab(evt, tabName, id) {
+function openTab(evt, tabName) {
 	var i, tabcontent, tablinks;
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
@@ -200,10 +129,11 @@ function openTab(evt, tabName, id) {
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " active";
 	
-	document.getElementById("tabState").value = id;
+	document.getElementById("tabState").value = tabName + "Button";
 }
 
 document.getElementById(document.getElementById("tabState").value).click();
+console.log(document.getElementById("tabState").value);
 
 </script>
    
